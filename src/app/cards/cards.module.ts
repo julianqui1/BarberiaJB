49b25b812da959eb5a72c2655a7c3503393c560b
Alex Betancur <brayamchic@gmail.com>
2024-11-25 22:30:00 -0500

    Creación de carpetas y componentes del proyecto

---
+++ src/app/cards/cards.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ServicioComponent } from './servicio/servicio.component';
+import { BebidaComponent } from './bebida/bebida.component';
+import { ProductoComponent } from './producto/producto.component';
+
+
+
+@NgModule({
+  declarations: [
+    ServicioComponent,
+    BebidaComponent,
+    ProductoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CardsModule { }
