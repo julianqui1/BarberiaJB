fe4a9cd2ea290f2a8e098c8bbe3b127be85e84ff
Alex Betancur <brayamchic@gmail.com>
2024-11-26 20:11:37 -0500

    Creacion del navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WelcomeComponent } from './components/welcome/welcome.component';
-import { PresentacionComponent } from './components/presentacion/presentacion.component';
-import { ServiciosComponent } from './components/servicios/servicios.component';
-import { BebidasComponent } from './components/bebidas/bebidas.component';
-import { HorarioComponent } from './components/horario/horario.component';
-import { UbicacionComponent } from './components/ubicacion/ubicacion.component';
-import { ProductosComponent } from './components/productos/productos.component';
+import { ComponentsModule } from './components/components.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WelcomeComponent,
-    PresentacionComponent,
-    ServiciosComponent,
-    BebidasComponent,
-    HorarioComponent,
-    UbicacionComponent,
-    ProductosComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ComponentsModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
