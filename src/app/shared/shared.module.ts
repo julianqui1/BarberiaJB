248a49976e4c1cd895ad4f7bfd27d27c2f379d5b
julianqui1 <julianquintero0111@gmail.com>
2024-12-13 21:37:25 -0500

    carrucel

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { CarrucelComponent } from './carrucel/carrucel.component';
+import { CardsModule } from '../cards/cards.module';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  declarations: [HeaderComponent, FooterComponent, CarrucelComponent],
+  imports: [CommonModule, CardsModule],
   exports: [HeaderComponent],
 })
 export class SharedModule {}
