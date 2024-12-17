1071c8d857a1ce7f73912b52875902e8d524d910
Alex Betancur <brayamchic@gmail.com>
2024-12-16 22:11:26 -0500

    Se avanza en los cambios del componente servicios

--- src/app/cards/servicio/servicio.component.ts
+++ src/app/cards/servicio/servicio.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Cortes } from 'src/app/models/cortes.interfaces';
+import { CORTES } from '../../data/cortes';
 
 @Component({
   selector: 'app-servicio',
   templateUrl: './servicio.component.html',
-  styleUrls: ['./servicio.component.css']
+  styleUrls: ['./servicio.component.css'],
 })
 export class ServicioComponent {
-
+  cortes: Cortes[] = CORTES;
 }
